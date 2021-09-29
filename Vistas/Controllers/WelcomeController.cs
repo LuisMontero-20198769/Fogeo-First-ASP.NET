50151eeb5331bf53d159cfcf3a94bc45aebfcbd1
LuisMontero20198769 <beisbol0208@hotmail.com>
2021-09-29 01:39:46 -0400

    Primera confirmacion

---
+++ Vistas/Controllers/WelcomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vistas.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Inicio()
+        {
+            //Bienvenida: Nombre de la App y Foto del ITLA
+            return View();
+        }
+
+        public ActionResult Pilares()
+        {
+            //Explicacion de los pilares de la programacion
+            return View();
+        }
+
+        public ActionResult Bosquejo()
+        {
+            return View();
+        }
+
+        public ActionResult Estudiante()
+        {
+            //Datos de Luis Angel Montero
+            return View();
+        }   
+    }
+}
